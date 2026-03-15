aa481794fb93c53bded08dc50d71aa7908f6476b
Jackson <jacksonbimenyimana3@gmail.com>
2026-03-15 17:21:18 +0200

    Added cors

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/v1": {
-        target: "http://localhost:8089",
+        target: process.env.VITE_API_BASE_URL || "http://localhost:8089",
         changeOrigin: true,
         secure: false,
       },
